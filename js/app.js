a98579c123860915ef38850d75808b5b5df6d11f
Victor Molina <victor98@ciencias.unam.mx>
2020-08-22 00:38:27 -0500

    Hamburguer menu

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const hamburguer = document.querySelector(".hamburguer");
+const navLinks = document.querySelector(".options");
+const logo = document.getElementById("WhiteLogo");
+
+hamburguer.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    source = logo.src;
+    if (source.includes("images/Atom/Icon/Logo/WhiteLogo.svg")) {
+        logo.src = "images/Atom/Icon/Logo/BlackLogo.svg";
+    } else {
+        logo.src = "images/Atom/Icon/Logo/WhiteLogo.svg";
+    }
+});
