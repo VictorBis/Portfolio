3e9f8b0995b66c3fa3c95fe65a3877d34f2def7e
Victor Molina <victor98@ciencias.unam.mx>
2020-08-22 16:02:31 -0500

    Hamburguer scroll bug fixed

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const logo = document.getElementById("WhiteLogo");
 
 hamburguer.addEventListener('click', () => {
     navLinks.classList.toggle('open');
+    document.getElementsByTagName('body')[0].classList.toggle('open');
     source = logo.src;
     if (source.includes("images/Atom/Icon/Logo/WhiteLogo.svg")) {
         logo.src = "images/Atom/Icon/Logo/BlackLogo.svg";
