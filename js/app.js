b4e9c335dae2ad0a1cd8c0429720f1ecd19c2083
Victor Molina <victor98@ciencias.unam.mx>
2020-08-24 22:39:37 -0500

    Hamburguer menu link opened bung fixed

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 const hamburguer = document.querySelector(".hamburguer");
 const navLinks = document.querySelector(".options");
 const logo = document.getElementById("WhiteLogo");
+const portfolioLink = document.getElementById("Portfolio");
 
 /** Hamburguer menu */
-hamburguer.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-    document.getElementsByTagName('body')[0].classList.toggle('open');
-    hamburguer.classList.toggle('open');
-    source = logo.src;
-    if (source.includes("images/Atom/Icon/Logo/WhiteLogo.svg")) {
-        logo.src = "images/Atom/Icon/Logo/BlackLogo.svg";
-    } else {
-        logo.src = "images/Atom/Icon/Logo/WhiteLogo.svg";
-    }
+[hamburguer, navLinks].forEach(item => {
+    item.addEventListener('click', () => {
+        navLinks.classList.toggle('open');
+        document.getElementsByTagName('body')[0].classList.toggle('open');
+        hamburguer.classList.toggle('open');
+        source = logo.src;
+        if (source.includes("images/Atom/Icon/Logo/WhiteLogo.svg")) {
+            logo.src = "images/Atom/Icon/Logo/BlackLogo.svg";
+        } else {
+            logo.src = "images/Atom/Icon/Logo/WhiteLogo.svg";
+        }
+    })
 });
