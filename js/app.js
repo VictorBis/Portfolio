e381df03d93b6e1f5c7715c858ed17b9414941c8
Victor Molina <victor98@ciencias.unam.mx>
2020-08-26 22:34:00 -0500

    Nav links bug fixed

--- js/app.js
+++ js/app.js
@@ -4,16 +4,24 @@ const logo = document.getElementById("WhiteLogo");
 const portfolioLink = document.getElementById("Portfolio");
 
 /** Hamburguer menu */
-[hamburguer, navLinks].forEach(item => {
-    item.addEventListener('click', () => {
-        navLinks.classList.toggle('open');
-        document.getElementsByTagName('body')[0].classList.toggle('open');
-        hamburguer.classList.toggle('open');
-        source = logo.src;
-        if (source.includes("images/Atom/Icon/Logo/WhiteLogo.svg")) {
-            logo.src = "images/Atom/Icon/Logo/BlackLogo.svg";
-        } else {
-            logo.src = "images/Atom/Icon/Logo/WhiteLogo.svg";
-        }
-    })
-});
+if (navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
+) {
+    [hamburguer, navLinks].forEach(item => {
+        item.addEventListener('click', () => {
+            navLinks.classList.toggle('open');
+            document.getElementsByTagName('body')[0].classList.toggle('open');
+            hamburguer.classList.toggle('open');
+            source = logo.src;
+            if (source.includes("images/Atom/Icon/Logo/WhiteLogo.svg")) {
+                logo.src = "images/Atom/Icon/Logo/BlackLogo.svg";
+            } else {
+                logo.src = "images/Atom/Icon/Logo/WhiteLogo.svg";
+            }
+        })
+    });
+}
