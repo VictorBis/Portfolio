6ef68534467f73b6b41e12bf56953af42068af23
Victor Molina <victor98@ciencias.unam.mx>
2021-02-20 19:01:12 -0600

    Updated Hero section

---
+++ js/typing.js
@@ -0,0 +1,14 @@
+var app = document.getElementById('typewriter');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+words = ['Hi', 'Hola','你好','Salut','こんにちは','Olá','Ciao'];
+
+words.forEach(element => {
+    typewriter.typeString(element+'!')
+    .pauseFor(1700)
+    .deleteAll()
+    .start();
+});
